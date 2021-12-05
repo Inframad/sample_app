065c0b94ee9afff00c7c465b113649efb61ad5ca
Тюкавкин Алексей Григорьевич <infinsinity@gmail.com>
2021-12-06 02:33:02 +0700

    Добавил viewBinding, обновил версии зависимостей

--- app/src/main/java/com/test/a2021_q4_tyukavkin/MainActivity.kt
+++ app/src/main/java/com/test/a2021_q4_tyukavkin/MainActivity.kt
@@ -2,10 +2,14 @@ package com.test.a2021_q4_tyukavkin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.a2021_q4_tyukavkin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
