bb894dcbb9e59604a479e664b31f5bad624747ce
Тюкавкин Алексей Григорьевич <infinsinity@gmail.com>
2021-12-17 23:22:03 +0700

    Упростил навигацию на фрагмент авторизации пользователя

--- app/src/main/java/com/test/a2021_q4_tyukavkin/ui/MainActivity.kt
+++ app/src/main/java/com/test/a2021_q4_tyukavkin/ui/MainActivity.kt
@@ -3,7 +3,7 @@ package com.test.a2021_q4_tyukavkin.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation
 import com.test.a2021_q4_tyukavkin.App
 import com.test.a2021_q4_tyukavkin.R
 import com.test.a2021_q4_tyukavkin.databinding.ActivityMainBinding
@@ -26,13 +26,11 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.isAuthorized.observe(this, {
-            if (it) {
-                if (savedInstanceState == null) {
-                    val navController = findNavController(R.id.my_nav_host_fragment)
-                    val graph = navController.navInflater.inflate(R.navigation.navigation)
-                    graph.startDestination = R.id.loans_history_dest
-                    navController.graph = graph
+        viewModel.isAuthorized.observe(this, { isAuthorized ->
+            if (!isAuthorized) {
+                Navigation.findNavController(binding.myNavHostFragment).apply {
+                    popBackStack()
+                    navigate(R.id.registration_dest)
                 }
             }
         })
